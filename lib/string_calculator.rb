957174a6b4ea211f01686d932eb9bc86fff25cff
tashoaf <tashoaf@gmail.com>
2023-12-03 20:52:08 +0000

    .gitignore file and tutorial learning ruby

---
+++ lib/string_calculator.rb
@@ -0,0 +1,11 @@
+class StringCalculator
+  
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      input.to_i
+    end
+  end
+  
+end
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,24 @@
+require "string_calculator"
+
+describe StringCalculator do
+
+  describe ".add" do
+    context "given an empty string" do
+      it "returns zero" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+
+    context "given '4'" do
+      it "returns 4" do
+        expect(StringCalculator.add("4")).to eq(4)
+      end
+    end
+    
+    context "given '10'" do
+      it "returns `10" do
+        expect(StringCalculator.add("10")).to eq(10)
+      end
+    end
+  end
+end
